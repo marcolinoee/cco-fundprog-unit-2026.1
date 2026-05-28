2ac621336275c0437d8ba84404a89e3c82899c62
Lucas Pedrosa <lucasrpedrosa25@gmail.com>
2026-05-28 14:56:41 -0300

    Update: Resolvendo conflitos

--- pratica_05/lucas-ribeiro/ex02_posicao_especifica.py
+++
@@ -1,8 +0,0 @@
-matriz = [
-    [10, 20, 30, 50],
-    [40, 50, 60, 60],
-    [70, 80, 90, 30]
-]
-
-print(f'Linha 1 e coluna 1: {matriz[0][0]}\nLinha 2 e coluna 3: {matriz[1][2]}\nLinha 3 e coluna 2: {matriz[2][1]}\nLinha 2 completa: {matriz[1]}')
-# A matriz[1][2] não representa a linha 1 e a coluna 2 pois na matriz se inicia em 0, portanto, tanto a linha quanto coluna tem como posição 1 o 0
--- pratica_05/lucas-ribeiro/ex04_percorrendo_matriz.py
+++
@@ -1,8 +0,0 @@
-matriz = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-for linha in matriz:
-    for n in linha:
-        print(f'Valor: {n}')
